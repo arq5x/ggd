cb7216901f24e3209103257dc6914d4bdd63a48e
arq5x <arq5x@virginia.edu>
2014-12-02 21:08:25 -0500

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+GGD. Test
+
+Examples:
+
+	# get CpG islands for human build 37 from UCSC 
+	$ python ggd.py install ucsc.human.b37.cpg
+	searcing for recipe: ucsc.human.b37.cpg
+	found recipe: ucsc.human.b37.cpg
+	installed ucsc.human.b37.cpg as ucsc.human.b37.cpg
+
+	# get CpG islands for human build 38 from UCSC and rename file
+	$ python ggd.py install ucsc.human.b38.cpg -o cpg.b38.bed
+	searcing for recipe: ucsc.human.b38.cpg
+	found recipe: ucsc.human.b38.cpg
+	installed ucsc.human.b38.cpg as cpg.b38.bed
+
+	# try to install a recipe that doesn't exist (nearly everything)
+	$ python ggd.py install ucsc.human.b38.dbsnp
+	searcing for recipe: ucsc.human.b38.dbsnp
+	could not find recipe: ucsc.human.b38.dbsnp
+	exiting.
---
+++ ggd.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import argparse
+import requests
+import subprocess
+
+recipe_urls = {
+  "core": "https://raw.githubusercontent.com/arq5x/ggd-recipes/master/",
+  }
+
+
+def _download_file(args, url):
+  """
+  http://stackoverflow.com/questions/16694907/\
+  how-to-download-large-file-in-python-with-requests-py
+  """
+  local_filename = url.split('/')[-1]
+  print >> sys.stderr, "searcing for recipe: " + args.recipe
+  r = requests.get(url, stream=True)
+  if r.status_code == 200:
+    print >> sys.stderr, "found recipe: " + args.recipe
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    return local_filename
+  else:
+    print >> sys.stderr, "could not find recipe: " + args.recipe
+    return None
+
+
+def _run_recipe(recipe, outfile):
+  f = open(outfile, 'w')
+  ret = subprocess.call(['bash', recipe], stdout=f)
+  return ret
+
+def install(parser, args):
+  # replace "." in recipe with slashes
+  recipe = args.recipe.replace('.', '/')
+
+  recipe_url = recipe_urls['core'] + recipe + '.sh'
+  recipe_file = _download_file(args, recipe_url)
+
+  if recipe_file is not None:
+    outfile = args.outfile 
+    if outfile is None:
+      outfile = args.recipe
+
+    failure = _run_recipe(recipe_file, outfile)
+    if not failure:
+      print >> sys.stderr, "installed " + args.recipe + " as " + outfile
+    else:
+      print >> sys.stderr, "failure installing " + args.recipe
+    os.remove(recipe_file)
+  else:
+    print >> sys.stderr, "exiting."
+
+def list(parser, args):
+  pass
+
+def main():
+
+  # create the top-level parser
+  parser = argparse.ArgumentParser(prog='ggd')
+  parser.add_argument("-v", "--version", help="Installed ggd version",
+                      action="version",
+                      version="alpha")
+  subparsers = parser.add_subparsers(title='[sub-commands]', dest='command')
+
+  # create the individual tool parsers
+  parser_install = subparsers.add_parser('install', 
+    help='Install a dataset based on a recipe')
+  parser_install.add_argument('recipe', 
+    metavar='STRING',
+    help='The GGD recipe to use.')
+  parser_install.add_argument('-o',
+    dest='outfile',
+    metavar='STRING',
+    required=False,
+    help='The name of the output file.')
+  parser_install.set_defaults(func=install)
+
+  # parse the args and call the selected function
+  args = parser.parse_args()
+
+  try:
+    args.func(parser, args)
+  except IOError, e:
+       if e.errno != 32:  # ignore SIGPIPE
+           raise
+
+if __name__ == "__main__":
+    main()
