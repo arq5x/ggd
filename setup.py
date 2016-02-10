29b9b48455fdd9376f59b561874558475e1b11e5
Brent Pedersen (brentp) <bpederse@gmail.com>
2016-02-10 07:44:00 -0700

    add nosetests

--- setup.py
+++ setup.py
@@ -15,23 +15,24 @@ setup(
         name="ggd",
         version=version,
         install_requires=install_requires,
-        requires = ['python (>=2.7, <3.0)'],
+        requires=['python (>=2.7, <3.0)'],
         packages=['ggd',
                   'ggd.scripts'],
         author="Aaron Quinlan",
         description='A toolset for getting genomics datasets',
         long_description=long_description,
         url="",
-        package_dir = {'ggd': "ggd"},
-        package_data = {'ggd': [], '': ['config.yaml']},
-        zip_safe = False,
+        package_dir={'ggd': "ggd"},
+        package_data={'ggd': [], '': ['config.yaml']},
+        zip_safe=False,
         include_package_data=True,
-        entry_points = {
-            'console_scripts' : [
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        entry_points={
+            'console_scripts': [
                  'ggd = ggd.ggd:main'
             ],
         },
-        scripts = ['ggd/scripts/query_biomart.py'],
         author_email="arq5x@virginia.edu",
         classifiers=[
             'Development Status :: 4 - Beta',
